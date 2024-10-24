f525aefebf7d6a695c3fc79d0b65ba767aa7d87e
Vidhi Patel <vpatel2@ualberta.ca>
2024-10-24 09:06:33 -0600

    test should fail

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -11,6 +11,40 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 
 public class CustomListTest {
+    /**
+     *This gets give of the list
+     * @return size of cities
+    */
+    public int getCount(){
 
+        return cities.size();
+    }
 
+    /*
+    This adds the city object to the list.
+    For the first phase, this will be empty
+    @param city
+    */
+    public void addCity(City city){
+
+    }
+
+    private CustomList list;
+
+    /**
+     * create a mock list for my citylist
+     * @return a mock city list
+     */
+    public CustomList MockCityList() {
+        list = new CustomList(null, new ArrayList<>());
+        return list;
+    }
+
+    @Test
+    public void addCityTest(){
+        list = MockCityList();
+        int listSize = list.getCount();
+        list.addCity(new City("Estevan", "SK"));
+        assertEquals(list.getCount(), listSize + 1);
+    }
 }
